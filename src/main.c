d5014956e10a20046430330394789ee77d33f747
rajmanna <raj2021manna@gmail.com>
2026-02-05 16:28:21 +0000

    handle exit builtin

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
@@ -10,6 +11,9 @@ int main(int argc, char *argv[]) {
     char command[256];
     if (fgets(command, sizeof(command), stdin)) {
       command[strcspn(command, "\n")] = 0;
+      if(strcmp(command, "exit") == 0) {
+        exit(0);
+      }
       printf("%s: command not found\n", command);
     }
   }
