45526910ee8b678fbfe16ebb675083eeb9f942a2
rajmanna <raj2021manna@gmail.com>
2026-02-06 08:34:09 +0000

    handle type command

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@ const char *BUILTINS[] = {"exit", "echo", "type", "cd", "pwd"};
 const int BUILTINS_LEN = sizeof(BUILTINS) / sizeof(BUILTINS[0]);
 
 int main(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
   setbuf(stdout, NULL);
 
   while (1) {
@@ -30,23 +32,35 @@ int main(int argc, char *argv[]) {
     }
 
     switch (pos) {
-      case 0:
-        exit(0);
-        break;
-      case 1:
-        printf("%s\n", command+5);
-        break;
-      case 2:
-        // execute type command
-        break;
-      case 3:
-        // execute cd command
-        break;
-      case 4:
-        // execute pwd command
-        break;
-      default:
-        continue;
+    case 0:
+      exit(0);
+      break;
+    case 1:
+      printf("%s\n", command + 5);
+      break;
+    case 2:
+      char *command_name = command + 5;
+      int is_valid = 0;
+      for (int i = 0; i < BUILTINS_LEN; i++) {
+        if (strncmp(command_name, BUILTINS[i], strlen(command_name)) == 0) {
+          is_valid = 1;
+          break;
+        }
+      }
+      if (is_valid) {
+        printf("%s is a shell builtin\n", command_name);
+      } else {
+        printf("%s: command not found\n", command_name);
+      }
+      break;
+    case 3:
+      // execute cd command
+      break;
+    case 4:
+      // execute pwd command
+      break;
+    default:
+      continue;
     }
   }
 
