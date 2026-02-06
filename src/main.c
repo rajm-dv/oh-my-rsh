69d29862263d5f08fb9634ed6433071fc95967f7
rajmanna <raj2021manna@gmail.com>
2026-02-06 08:17:59 +0000

    handle echo command

--- src/main.c
+++ src/main.c
@@ -2,19 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+const char *BUILTINS[] = {"exit", "echo", "type", "cd", "pwd"};
+const int BUILTINS_LEN = sizeof(BUILTINS) / sizeof(BUILTINS[0]);
+
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
   while (1) {
     printf("$ ");
 
-    char command[256];
-    if (fgets(command, sizeof(command), stdin)) {
-      command[strcspn(command, "\n")] = 0;
-      if(strcmp(command, "exit") == 0) {
-        exit(0);
+    char command[1024];
+    if (fgets(command, sizeof(command), stdin) == NULL) {
+    }
+
+    *strchr(command, '\n') = '\0';
+    int found = 0, pos = -1;
+    for (int i = 0; i < BUILTINS_LEN; i++) {
+      if (strncmp(command, BUILTINS[i], strlen(BUILTINS[i])) == 0) {
+        found = 1;
+        pos = i;
+        break;
       }
+    }
+
+    if (!found) {
       printf("%s: command not found\n", command);
+    }
+
+    switch (pos) {
+      case 0:
+        exit(0);
+        break;
+      case 1:
+        printf("%s\n", command+5);
+        break;
+      case 2:
+        // execute type command
+        break;
+      case 3:
+        // execute cd command
+        break;
+      case 4:
+        // execute pwd command
+        break;
+      default:
+        continue;
     }
   }
 
